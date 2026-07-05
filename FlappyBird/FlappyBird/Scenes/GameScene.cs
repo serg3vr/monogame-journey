a3764ff1c0c0655dd11835e9b0354ed7d2d30263
Sergio Valdez <sergioavr93@gmail.com>
2026-07-05 10:28:07 -0600

    Add game over

--- FlappyBird/FlappyBird/Scenes/GameScene.cs
+++ FlappyBird/FlappyBird/Scenes/GameScene.cs
@@ -14,8 +14,8 @@ namespace FlappyBird;
 public class GameScene : Scene
 {
     private static readonly Random _random = new();
-    private const float GRAVITY = 1200f;
-    private const float IMPULSE = 550f;
+    private const float GRAVITY = 1500f;
+    private const float IMPULSE = 480f;
 
     private float _usableScreenWidth;
 
@@ -141,22 +141,24 @@ public class GameScene : Scene
         _bird.SpriteColor = Color.Yellow;
         _bird.Speed = 1f;
 
-        var size = _random.Next(3, 6);
+        var size = GetNextGap();
         var otherSize = 11 - size - 3;
 
-        var pipe1 = new Pipe(_texture, new Vector2(100, 0), new Vector2(64, size * 64));
+        var initialX = 256;
+
+        var pipe1 = new Pipe(_texture, new Vector2(initialX + 100, 0), new Vector2(64, size * 64));
         pipe1.SpriteColor = Color.Green;
-        var pipe2 = new Pipe(_texture, new Vector2(100, Globals.ScreenHeight - otherSize * 64), new Vector2(64, otherSize * 64));
+        var pipe2 = new Pipe(_texture, new Vector2(initialX + 100, Globals.ScreenHeight - otherSize * 64), new Vector2(64, otherSize * 64));
         pipe2.SpriteColor = Color.Green;
         _pipes.Add(pipe1);
         _pipes.Add(pipe2);
 
-        size = _random.Next(3, 6);
+        size = GetNextGap();
         otherSize = 11 - size - 3;
 
-        var pipe3 = new Pipe(_texture, new Vector2(356, 0), new Vector2(64, size * 64));
+        var pipe3 = new Pipe(_texture, new Vector2(initialX + 356, 0), new Vector2(64, size * 64));
         pipe3.SpriteColor = Color.Green;
-        var pipe4 = new Pipe(_texture, new Vector2(356, Globals.ScreenHeight - otherSize * 64), new Vector2(64, otherSize * 64));
+        var pipe4 = new Pipe(_texture, new Vector2(initialX + 356, Globals.ScreenHeight - otherSize * 64), new Vector2(64, otherSize * 64));
         pipe4.SpriteColor = Color.Green;
         _pipes.Add(pipe4);
         _pipes.Add(pipe3);
@@ -208,11 +210,7 @@ public class GameScene : Scene
         }
 
         if (_pipes[0].Bounds.Right < 0) {
-            // 1,2,3,4,5,6,7,8,9,10,11
-            // 1,2,0,0,0,6,7,8,9,10,11
-            // 1,2,3,4,5,6,0,0,0,10,11
-
-            var size = _random.Next(2, 7);
+            var size = GetNextGap();
             var otherSize = 11 - size - 3;
             _pipes[0].Position = new Vector2(Globals.ScreenWidth + _pipes[0].Bounds.Width, 0);
             _pipes[0].Size = new Vector2(64, size * 64);
@@ -222,13 +220,30 @@ public class GameScene : Scene
         }
 
         if (_pipes[2].Bounds.Right < 0) {
-            var size = _random.Next(2, 7);
+            var size = GetNextGap();
             var otherSize = 11 - size - 3;
             _pipes[2].Position = new Vector2(Globals.ScreenWidth + _pipes[2].Bounds.Width, 0);
             _pipes[2].Size = new Vector2(64, size * 64);
 
             _pipes[3].Position = new Vector2(Globals.ScreenWidth + _pipes[3].Bounds.Width, Globals.ScreenHeight - otherSize * 64);
             _pipes[3].Size = new Vector2(64, otherSize * 64);
+        }
+
+        var wasCollision = false;
+
+        foreach (var pipe in _pipes) {
+            if (_bird.Bounds.Intersects(pipe.Bounds)) {
+                wasCollision =  true;
+                break;
+            }
+        }
+
+        if (_bird.Bounds.Top < 0 || _bird.Bounds.Bottom > Globals.ScreenHeight) {
+            wasCollision =  true;
+        }
+
+        if (wasCollision) {
+            _isGameOver = true;
         }
 
         _previousKeyboardState = ks;
@@ -257,5 +272,10 @@ public class GameScene : Scene
         }
 
         SpriteBatch.End();
+    }
+
+    private int GetNextGap()
+    {
+        return _random.Next(2, 7);
     }
 }
